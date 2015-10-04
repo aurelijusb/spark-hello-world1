9408806f1423974fdc642020ca74a92321df46dd
Aurelijus Banelis <aurelijus@banelis.lt>
2015-10-04 22:24:23 +0300

    Top10 words in README.md

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -6,13 +6,16 @@ import org.apache.spark.SparkConf
 object SimpleApp {
   def main(args: Array[String]) {
     val logFile = "README.md"
-    val hostName = java.net.InetAddress.getLocalHost.getHostName
-    val conf = new SparkConf().setAppName("Test1").setMaster(s"spark://$hostName:7077")
+//    val hostName = java.net.InetAddress.getLocalHost.getHostName
+    val conf = new SparkConf().setAppName("Test1").setMaster("local[2]") ///.setMaster(s"spark://$hostName:7077")
     val sc = new SparkContext(conf)
     sc.addJar("target/scala-2.10/hello-spark-1_2.10-1.0.jar")
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
+    val wordCounts = logData.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+    val top10 = wordCounts.sortBy[Int](_._2, ascending = false).take(10)
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    println("WordCount:\n" + top10.map(pair => s"'${pair._1}': ${pair._2}").mkString("\n"))
   }
 }
